58f537f627491456e97d3dfc17e49827e40c8462
Nikola  Zezulová <nikola.zezulova99@gmail.com>
2018-06-08 13:58:38 +0200

    nic

--- fakto.pas
+++ fakto.pas
@@ -3,15 +3,20 @@ Uses sysutils;
 
 
 
-function faktocyklus():integer;
-var
+function faktocyklus(n:integer):integer;
+var 
+i:integer;
 begin
+	if (n=0) then faktocyklus:=1
+	else if (n=1) then faktocyklus:=1 
+	else
  for i:= n downto 1 do begin
- 
+ faktocyklus:= faktocyklus(n-1)*n;
+end;
 end;
 
 
-function faktoreku():integer;
+function faktoreku(n:integer):integer;
 begin
 if
 	(n=0) then faktoreku:=1 else 
@@ -22,8 +27,12 @@ end;
 
 
 
-
+var
+n:integer;
 begin
 n:=StrToInt(ParamStr(1));
-writeln(faktoreku(1));
+x:= StrToInt(ParamStr(2));
+if (x=0) then writeln(faktoreku(n)) else
+	if (x=1) then writeln(faktocyklus(n))
+	else writeln("seš langoš");
 end.
